268f3742863b1381162f3c22c5093691b150cc77
HuyLN <lynhathuy38@gmail.com>
2024-01-04 22:10:11 +0700

    Updating Mouse and KeyBoard Input

---
+++ src/inputs/KeyboardInputs.java
@@ -0,0 +1,47 @@
+package inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e){
+
+       }
+
+       @Override
+          public void keyPressed(Ke
+    
+
+       
+       @Override
+          public void keyReleased(KeyEvent
+
+     
+
+           ca
+               System.out.println("Its W"
+              case KeyEvent.VK_A:
+
+     
+
+           ca
+               System.out.println("Its D
+
+              
+              }
+                
+                
+            
+                
+                
+            
+                
+                
+            
+                
+                
+        
+
+    
